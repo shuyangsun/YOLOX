346e3745910ac2d47e4d6e1cf3034391f898fb9a
Shuyang Sun <shuyangsun10@gmail.com>
2023-09-30 19:07:33 -0400

    bug fix.

--- tools/trt.py
+++ tools/trt.py
@@ -46,8 +46,8 @@ def prepare_samples(
             f_lower = f.lower()
             if (
                 not f_lower.endswith(".png")
-                or f_lower.endswith(".jpg")
-                or f_lower.endswith(".jpeg")
+                and not f_lower.endswith(".jpg")
+                and not f_lower.endswith(".jpeg")
             ):
                 continue
             img = cv2.imread(os.path.join(root, f))
